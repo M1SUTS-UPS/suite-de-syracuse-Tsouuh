0e38f4b6644c5817c38697e3d23d69ac354580f7
rhm2875a <rhm2875a@3tp1-h2-08d.univ-tlse3.fr>
2024-09-16 17:07:36 +0200

    Ajout de mon nom

---
+++ syracuse.c
@@ -0,0 +1,18 @@
+/*Ginoux Laëtitia & Raharijaona-Ndrianarilala Mandimbihaja Onenantsoa*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {     
+	double u,un;
+	
+	scanf("choisissez une valeur de u0 supérieur à 1: %lf\n",&u);
+	while (u < 1){
+		scanf("choisissez une valeur de u0 supérieur à 1: %lf\n",&u);
+	}
+
+	
+}
+
