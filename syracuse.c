bd68d707f6d77554724b2d5d9ec14ef4ad9d14fb
rhm2875a <rhm2875a@3tp1-h2-08d.univ-tlse3.fr>
2024-09-16 17:29:42 +0200

    Modification de la fonction

--- syracuse.c
+++ syracuse.c
@@ -6,13 +6,28 @@
 
 
 int main () {     
-	double u,un;
-	
-	scanf("choisissez une valeur de u0 supérieur à 1: %lf\n",&u);
+	int u, un, i=0;
+	printf("choisissez une valeur de u0 supérieur à 1:\n");
+	scanf("%d\n",&u);
 	while (u < 1){
-		scanf("choisissez une valeur de u0 supérieur à 1: %lf\n",&u);
+		printf("choisissez une valeur de u0 supérieur à 1:\n");
+		scanf("%d\n",&u);
 	}
-
-	
+	while ( u > 1 ){
+		if (u%2==0){
+		
+			un = u/2;
+			u  = un;
+			i  = i+1;
+		}
+		else {
+		
+			un = 3*u + 1;
+			u  = un;
+			i  = i+1;
+		}
+		
+		printf("%d",u);
+	}
 }
 
