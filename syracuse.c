bdd2905e0b093207e393971709be028d860aa6b8
rhm2875a <rhm2875a@u6-100-20d.univ-tlse3.fr>
2024-09-23 14:40:02 +0200

    Syracuse fini

--- syracuse.c
+++ syracuse.c
@@ -4,16 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void syracuse(int u, int un, int i, int max);
+
+
+
+
+
+
+
+
+
+
 
 int main () {     
-	int u, un, i=0;
-	printf("choisissez une valeur de u0 supérieur à 1:\n");
-	scanf("%d\n",&u);
+	int u=0, un=0, i=0, max=0 ;
+	
 	while (u < 1){
-		printf("choisissez une valeur de u0 supérieur à 1:\n");
-		scanf("%d\n",&u);
+		printf("choisissez une valeur entière de u0 supérieur à 1:\n");
+		scanf("%d",&u);
 	}
+	
+	syracuse(u, un, i, max);
+	
+}
+
+void syracuse(int u, int un, int i, int max){
+
+	FILE *file;
+	
+	file = fopen("syracuse.txt","w");
+
+	fprintf(file,"%d	%d\n",i,u);
 	while ( u > 1 ){
+		if (u >= max){max=u;}
 		if (u%2==0){
 		
 			un = u/2;
@@ -26,8 +49,15 @@ int main () {
 			u  = un;
 			i  = i+1;
 		}
+		fprintf(file,"%d	%d\n",i,u);
 		
-		printf("%d",u);
+		
 	}
+	printf("Le nombre d'itération est %d\n",i);
+	printf("L'altitude est %d\n",max);
+	
+	fclose(file);
+
+
 }
 
